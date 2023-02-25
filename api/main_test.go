90d14b5644b7575413280f72e33adb3e7c9d3206
Luis Rosales <eng.luisrosales@gmail.com>
2023-02-25 19:12:52 +0100

    feat: added login route and tests, grouped secured routes

--- api/main_test.go
+++ api/main_test.go
@@ -2,11 +2,26 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	db "github.com/luislard/simplebank/db/sqlc"
+	"github.com/luislard/simplebank/util"
+	"github.com/stretchr/testify/require"
 	"os"
 	"testing"
+	"time"
 )
 
 func TestMain(m *testing.M) {
 	gin.SetMode(gin.TestMode)
 	os.Exit(m.Run())
+}
+
+func newTestServer(t *testing.T, store db.Store) *Server {
+	config := util.Config{
+		TokenSymmetricKey:   util.RandomString(32),
+		AccessTokenDuration: time.Minute,
+	}
+
+	server, err := NewServer(config, store)
+	require.NoError(t, err)
+	return server
 }
